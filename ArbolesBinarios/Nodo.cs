f58fb410bede4def0c64b46ed33485c85c928768
Raymundo Daniel Ramírez Ramos <ray.ramirez@unikino.edu.mx>
2022-06-16 08:10:31 -0700

    agregue como nuevo proyecto arbolesbinarios

---
+++ ArbolesBinarios/Nodo.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArbolesBinarios
+{
+    public class Nodo
+    {
+        private int dato;
+        private Nodo subArbolIzquierdo;
+        private Nodo subArbolDerecho;
+
+        public int Dato { get => dato; set => dato = value; }
+        public Nodo SubArbolIzquierdo { get => subArbolIzquierdo; set => subArbolIzquierdo = value; }
+        public Nodo SubArbolDerecho { get => subArbolDerecho; set => subArbolDerecho = value; }
+
+        public Nodo(int dato, Nodo subArbolIzquierdo = null, Nodo subArbolDerecho = null)
+        {
+            this.dato = dato;
+            this.subArbolIzquierdo = subArbolIzquierdo;
+            this.subArbolDerecho = subArbolDerecho;
+        }
+    }
+}
---
+++ ArbolesBinarios/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ArbolesBinarios
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArbolBinarioBusqueda abb = new ArbolBinarioBusqueda(120);
+
+            int[] numeros = { 87, 140, 43, 99, 130, 22, 65, 56, 93, 135, 10, 150, 1, 150, 2000 };
+
+            foreach (int numero in numeros)
+            {
+                abb.Insertar(numero);
+            }
+
+            Console.WriteLine(abb.ObtenerArbol());
+
+
+            Console.WriteLine("");
+            Console.WriteLine(abb.Recorrido(tipoRecorrido: ArbolBinarioBusqueda.TipoRecorrido.Preorden));
+            Console.WriteLine(abb.Recorrido(tipoRecorrido: ArbolBinarioBusqueda.TipoRecorrido.Inorden));
+            Console.WriteLine(abb.Recorrido(tipoRecorrido: ArbolBinarioBusqueda.TipoRecorrido.Posorden));
+        }
+    }
+}
